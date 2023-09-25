313578359cb7eb42c639d938800b711563ee776a
Yahia Ibrahim <Yahiasheriif87@gmail.com>
2023-09-25 03:17:40 +0300

    inital commit

---
+++ PasswordValid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+internal class Program
+{
+    static bool IsPasswordValid(string password)
+    {
+        return password.Length >= 8 && password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit);
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(IsPasswordValid("Hello123@"));
+        Console.WriteLine(IsPasswordValid("hello123@"));
+        Console.WriteLine(IsPasswordValid("HELLO123@"));
+        Console.WriteLine(IsPasswordValid("Hello123"));
+        Console.WriteLine(IsPasswordValid("Hello@"));
+
+
+
+    }
+}
